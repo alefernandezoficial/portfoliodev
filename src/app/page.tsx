2ddcdb5980f4b9fdf7b46fd2c70a67c1521809cc
alefernandezoficial <alefernandezoficial@gmail.com>
2023-12-30 13:12:32 -0300

    master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Skills from "./components/Skills";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 function page() {
   return (
     <main>
+
+
+      <div className="container-fluid">
+        <div className="container-central">
+          <p className="text-center">
+            <span className="badge text-bg-success">Curriculum Vitae</span>
+          </p>
+          <p className="text-center">
+            <Link href="pdf/cv.pdf"><button className={styles.glowingbtn}><span className={styles.glowingtxt}>D<span className={styles.faultyletter}>O</span>WNLOAD</span></button></Link>
+          </p>
+
+        </div>
+      </div>
+
       <Skills />
     </main>
   );
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,72 +5,38 @@ function page() {
   return (
     <main>
       <div className='container'>
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="goatburger" />
-              <div className="card-body">
-                <h5 className="card-title">GoatBurger - Hamburguesas</h5>
-                <p className="card-text">Pagina 1 Page centrada en el comercio electronico de una hamburgueseria
-                  con sus respectivos menus en base de datos SupaBase, banners de publicidad
-                  con photoshop, simple, rapido y centrado en el punto base, las hamburguesas.</p>
-                <p className="card-text">
-                  <small className="text-body-secondary">
-                    HTML, CSS, JavaScript, ReactJS & Bootstrap
-                  </small>
-                </p>
-                <a href="https://alefernandezoficial.github.io/my-hamburger/"><i className="bi bi-link-45deg"></i>Ir a la pagina</a>
-              </div>
-            </div>
+
+        <div className="row">
+          <div className="col-md-7">
+            <a href="#">
+              <img className="img-fluid rounded mb-3 mb-md-0" src="/image/goatburger.png" alt="goatburger" />
+            </a>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="sickgamers" />
-              <div className="card-body">
-                <h5 className="card-title">SickGamers - Ecommerce</h5>
-                <p className="card-text">Pagina de 6 secciones con informacion y compra de Accesorios gamers
-                e Indumentaria de alta calidad e importadas de afuera, tematica simple
-                accesible y rapida, photoshop para las ilustraciones y logo, enfocada
-                en la venta de productos electronicos e indumentaria.</p>
-                <p className="card-text">
-                  <small className="text-body-secondary">
-                    HTML, CSS, JavaScript, ReactJS & Bootstrap
-                  </small>
-                </p>
-                <a href="https://alefernandezoficial.github.io/my-ecommerce/"><i className="bi bi-link-45deg"></i>Ir a la pagina</a></div>
+          <div className="col-md-5">
+            <h3><span className="badge bg-secondary">GoatBurger - Hamburguesas</span></h3>
+            <div className="alert alert-secondary" role="alert">
+            <p>1 Page centrada en el comercio electronico de una hamburgueseria con sus respectivos menus en base de datos SupaBase, banners de publicidad con photoshop, simple, rapido y centrado en el punto base, las hamburguesas.</p>
             </div>
+            <a className="btn btn-primary" href="https://alefernandezoficial.github.io/my-hamburger/">Ver proyecto</a>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="vinoteca" />
-              <div className="card-body">
-                <h5 className="card-title">Ejemplo 3</h5>
-                <p className="card-text">Pagina 1 Page centrada en el comercio electronico de una hamburgueseria
-                  con sus respectivos menus en base de datos SupaBase, banners de publicidad
-                  con photoshop, simple, rapido y centrado en el punto base, las hamburguesas.</p>
-                <p className="card-text">
-                  <small className="text-body-secondary">
-                    HTML, CSS, JavaScript, ReactJS & Bootstrap
-                  </small>
-                </p>
-                <a href="https://alefernandezoficial.github.io/my-hamburger/"><i className="bi bi-link-45deg"></i>Ir a la pagina</a></div>
-            </div>
+        </div>
+
+        {/* Divider */}
+        <hr />
+        {/* Divider */}
+
+        <div className="row">
+          <div className="col-md-7">
+            <a href="#">
+              <img className="img-fluid rounded mb-3 mb-md-0" src="/image/sickgamers.png" alt="sickgamers" />
+            </a>
           </div>
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="peliculas" />
-              <div className="card-body">
-                <h5 className="card-title">Ejemplo 4</h5>
-                <p className="card-text">Pagina 1 Page centrada en el comercio electronico de una hamburgueseria
-                  con sus respectivos menus en base de datos SupaBase, banners de publicidad
-                  con photoshop, simple, rapido y centrado en el punto base, las hamburguesas.</p>
-                <p className="card-text">
-                  <small className="text-body-secondary">
-                    HTML, CSS, JavaScript, ReactJS & Bootstrap
-                  </small>
-                </p>
-                <a href="https://alefernandezoficial.github.io/my-hamburger/"><i className="bi bi-link-45deg"></i>Ir a la pagina</a></div>
+          <div className="col-md-5">
+            <h3><span className="badge bg-secondary">SickGamers - Ecommerce</span></h3>
+            <div className="alert alert-secondary" role="alert">
+            <p>6 secciones con informacion y compra de Accesorios gamers e Indumentaria de alta calidad e importadas de afuera, tematica simple accesible y rapida, photoshop para las ilustraciones y logo, enfocada en la venta de productos electronicos e indumentaria.</p>
             </div>
+            <a className="btn btn-primary" href="https://alefernandezoficial.github.io/my-ecommerce/">Ver proyecto</a>
           </div>
         </div>
 
